73ee4a6215238543fcb88555e4c073d4930971ac
Abdul Wahid <rtiabdulwahid@gmail.com>
2025-11-17 15:57:36 +0530

    fix: Update authentication logic to allow access to specific public pages; remove unnecessary conditions

--- src/app/Auth.jsx
+++ src/app/Auth.jsx
@@ -9,7 +9,7 @@ export default function Auth({ children }) {
 
   useEffect(() => {
     // Allow public page(s)
-    if (pathname === '/verify-certificate' || pathname === '/login' || pathname === '/termsandconditions/C4B/t&c' || pathname === `/termsandconditions/DM/t&c` || pathname === '/termsandconditions/C4B'|| pathname === '/termsandconditions/C4B/onboard' || pathname === '/termsandconditions/C4B/pdf' || pathname === '/termsandconditions/C4B/offboard' || pathname === `/termsandconditions/FSD/t&c`) return;
+    if (pathname === '/verify-certificate' || pathname === '/login' || pathname === '/termsandconditions/C4B/t&c' || pathname === '/termsandconditions/C4B'|| pathname === '/termsandconditions/C4B/onboard' || pathname === '/termsandconditions/C4B/pdf' || pathname === '/termsandconditions/C4B/offboard') return;
 
     const isAuthenticated = sessionStorage.getItem("isAuthenticated");
     if (!isAuthenticated) {
